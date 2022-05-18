bcdc0cf74e2408e06d481c6a03baa8da2870b453
shermaine <shermaine.lim.si.hui@gmail.com>
2022-05-18 15:06:17 +0800

    add modal

--- OverworldMap.js
+++ OverworldMap.js
@@ -114,7 +114,6 @@ function clicked2(e) {
 
 const city = document?.querySelector(".city");
 const modal = document.querySelector(".modal");
-const trigger = document.querySelector(".trigger");
 const closeButton = document.querySelector(".close-button");
 
 function toggleModal() {
@@ -130,6 +129,47 @@ function windowOnClick(event) {
 city.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
+
+//moon
+const moon = document?.querySelector(".moon");
+const modal2 = document.querySelector(".modal2");
+
+const closeButton2 = document.querySelector(".close-button2");
+
+function toggleModal2() {
+  modal2.classList.toggle("show-modal2");
+}
+
+function windowOnClick2(event) {
+  if (event.target === modal2) {
+    toggleModal2();
+  }
+}
+
+moon.addEventListener("click", toggleModal2);
+closeButton2.addEventListener("click", toggleModal2);
+window.addEventListener("click", windowOnClick2);
+
+//cityDay
+
+const cityDay = document?.querySelector(".cityDay");
+const modal3 = document.querySelector(".modal3");
+
+const closeButton3 = document.querySelector(".close-button3");
+
+function toggleModal3() {
+  modal3.classList.toggle("show-modal3");
+}
+
+function windowOnClick3(event) {
+  if (event.target === modal3) {
+    toggleModal3();
+  }
+}
+
+cityDay.addEventListener("click", toggleModal3);
+closeButton3.addEventListener("click", toggleModal3);
+window.addEventListener("click", windowOnClick3);
 
 window.OverworldMaps = {
   Kitchen: {
